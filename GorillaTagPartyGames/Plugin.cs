ee08b06c1771da1917f4528fe10b224144bf8298
pl2w <dc.pl2w@gmail.com>
2026-01-23 15:33:37 +0200

    Initial commit

---
+++ GorillaTagPartyGames/Plugin.cs
@@ -0,0 +1,13 @@
+﻿namespace GorillaTagPartyGames;
+
+[BepInPlugin(PluginInfo.Guid, PluginInfo.Name, PluginInfo.Version)]
+public class Plugin : BaseUnityPlugin
+{
+}
+
+public static class PluginInfo
+{
+    public const string Guid = "xyz.pl2w.gtag.partygames";
+    public const string Name = "Gorilla Tag Party Games";
+    public const string Version = "0.1.0";
+}
