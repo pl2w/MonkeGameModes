0d390dfd92d0f8b9c5f91a34e170cc61df8f5a28
pl2w <dc.pl2w@gmail.com>
2026-01-24 23:28:15 +0200

    commit

---
+++ MonkeUtils/Plugin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MonkeUtils;
+
+[BepInPlugin(PluginInfo.Guid, PluginInfo.Name, PluginInfo.Version)]
+[BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+[ModdedGamemode(GameModeInfo.TeamTagGuid, GameModeInfo.TeamTagName, typeof(TeamTag))]
+public class Plugin : BaseUnityPlugin
+{
+    public Plugin()
+    {
+        var harmony = new Harmony(PluginInfo.Guid);
+        harmony.PatchAll(Assembly.GetExecutingAssembly());
+    }
+}
+
+public static class PluginInfo
+{
+    public const string Guid = "xyz.pl2w.gtag.partygames";
+    public const string Name = "Gorilla Tag Party Games";
+    public const string Version = "0.1.0";
+}
